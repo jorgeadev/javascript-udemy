ab9d76612a98b434b0879ff5295f8ec2a7c3c1fc
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2020-05-12 23:05:36 -0500

    Create string.js

---
+++ pruebaJS/primeros_pasos/string.js
@@ -0,0 +1,9 @@
+var nombre = "Jorge"
+var edad = 45
+
+console.log("Mi nombre es: " + nombre + " y tengo " +  edad + " años.")
+console.log(`Mi nombre es: ${nombre} y tengo ${edad} años.`)
+
+console.log(nombre.toUpperCase())
+console.log(nombre.toLowerCase())
+console.log(nombre.length)
