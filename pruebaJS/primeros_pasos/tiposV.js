37a5b9b038f998b8bca48f9deb28ddfa36f75c00
Jorge Alberto Gomez Gomez <36772740+jorgealbertogomezgomez77@users.noreply.github.com>
2020-05-12 23:05:30 -0500

    Create tiposV.js

---
+++ pruebaJS/primeros_pasos/tiposV.js
@@ -0,0 +1,30 @@
+// String
+var nombre = "Jorge"
+console.log(nombre)
+
+// Number
+var miNumero = 50
+console.log(miNumero)
+
+//decimal
+//const PI = 3.141516
+//console.log(PI)
+
+// Booleano
+var verdadero = true
+var falso = false
+console.log(verdadero)
+console.log(falso)
+
+// null
+var nul0 = null
+console.log(nul0)
+
+// undefined
+var indefinido = undefined
+console.log(indefinido)
+
+console.log(typeof verdadero)
+
+const PI = Math.PI
+console.log(PI)
